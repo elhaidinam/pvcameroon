6184150714b1d25f0101025b678721742e1815ba
Edinam <elhaidinam@gmail.com>
2026-05-14 13:09:54 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple pass-through for now, but required for PWA installability on Chrome
+  event.respondWith(fetch(event.request));
+});
